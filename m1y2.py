a4ba2389680d94bf735167181d1a4c94255155b3
ilbro25 <57097129+ilbro25@users.noreply.github.com>
2023-09-24 13:17:56 +0300

    Update m1y2.py

--- m1y2.py
+++ m1y2.py
@@ -1,8 +1,48 @@
 import random
-sym = "abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.,/_-=+?!@#$%^&*()"
-while (True):
-    leight = int(input("Введите желаемую длину пароля: "))
+import discord 
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+def pass_gen(lenpassword):
+    elements = "abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.,/_-=+?!@#$%^&*()"
     password = ""
-    for i in range(leight):
-        password += random.choice(sym)
-    print(password)
+    for i in range(lenpassword):
+        password += random.choice(elements)
+    return password
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send(f'Пока')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command()
+async def password(ctx, count = 10):
+    await ctx.send(f'Ваш пароль - {pass_gen(count)}')
+
+bot.run("Token")
